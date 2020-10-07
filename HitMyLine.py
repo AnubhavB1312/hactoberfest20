decfb3f33481951c0a1ff30671cdc4653feb5e7c
Siddharth Rana <56639199+thesidrana@users.noreply.github.com>
2020-10-07 11:51:54 +0530

    Create HitMyLine.py
    Displays song lyrics line by line

---
+++ HitMyLine.py
@@ -0,0 +1,30 @@
+Loader = "";
+print ("HIT MY LINE - LOGIC\n")
+lyr = ['''Now I ain't sayin' this my "Jesus Walks"''',
+'''I'm just sayin' God, I need to talk''',
+'''Too many kids in the community outlined in chalk''',
+'''Scared of drive-bys when they should just be scared of the dark''',
+'''Who's really doin' they part?''',
+'''They say they don't want messages in rap, it ruins the art''',
+'''Well here I am people, yeah, now tear me apart''',
+'''So much happenin' in the world I can't touch on every topic''',
+'''I know you hear it in my voice, make the devil stop it''',
+'''It's been a long time, God, can you hit my line?''',
+'''Too many people dyin' and baby mamas cryin''',
+'''Evil politicians, people on Twitter bitchin' ''',
+'''Hashtaggin' but in real life they never pitch in''',
+'''I came with conviction, I hope you came to listen''',
+'''No matter how much I ignore it, this message my mission ''',
+'''God hit my line, this world is in critical condition ''',
+'''Like a fiend that need fixin', that done battled addiction ''',
+'''Been addicted to money, been addicted to fame ''',
+'''Addicted to arenas and people screamin' my name ''',
+'''But it's time to let go, it's time to make a change ''',
+'''So can you hit my line? I know you hear this rhyme ''',
+'''But come to think of it, you probably hear this all the time ''',
+'''Probably hear this all the time ''']
+i = 0
+for i in range(0,len(lyr)):
+    import time
+    print(lyr[i])
+    time.sleep(2.0)
